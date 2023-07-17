49e8143d9c31c34bd64bfcf0f004d884e19b9919
jungwoogyun <jwg8910@naver.com>
2023-07-17 13:43:37 +0900

    'ok'

--- src/main/webapp/JS/Regis_Cancle.js
+++
@@ -1,6 +0,0 @@
-/**@charset "UTF-8";
- * 
- */
- document.getElementById("cancle").addEventListener("click",function(){
-	 window.location.href="./Login.jsp";
- });
--- src/main/webapp/JS/Register.js
+++
@@ -1,8 +0,0 @@
-/**@charset "UTF-8";
- * 
- */
- 
- // 로그인 버튼 클릭 이벤트 핸들러
- document.getElementById("regis").addEventListener('click',function(){
-	 window.location.href = "./Register.jsp";
- })
